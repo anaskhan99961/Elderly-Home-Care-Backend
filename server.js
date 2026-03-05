806ffef20851b4885884617f83700ecae9834324
Anees kaim Khani <anees.sardar@salesflo.com>
2026-03-05 20:24:28 +0500

    first commit

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.use(express.json());
 app.use('/api', routes);
 app.get('/api/health', (req, res) => res.json({ ok: true }));
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the Elderly Home Care API');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
